1a2ad70bb98439b929d03382cbbdd265f55d0d78
DotheZac <128376783+DotheZac@users.noreply.github.com>
2025-10-24 20:35:04 +0900

    yuighih;hjk;n

---
+++ Model/Model.cpp
@@ -0,0 +1,48 @@
+﻿#include "DX11.h"
+#include "Model.h"
+// TODO: 라이브러리 함수의 예제입니다.
+
+Model::Model()
+{
+	m_Name = L"N/A";
+	m_buffSize = 0;
+	m_Stride = 0;
+	m_Offset = 0;
+	m_VtxCnt = 0;
+}
+
+Model::~Model()
+{
+}
+
+const TCHAR* Model::SetName(const TCHAR* name)
+{
+	m_Name = name;
+	return m_Name;
+}
+
+int Model::Create(ID3D11Device* pDev, VOID* pBuff, UINT size)
+{
+	int res = S_OK;
+	m_pDev = pDev;
+	m_pDev->GetImmediateContext(m_pDXDC.GetAddressOf());
+
+
+
+	return 0;
+}
+
+int Model::Update(float dTime)
+{
+	return 0;
+}
+
+int Model::Draw(float dTime, DRAWTYPE drawType)
+{
+	return 0;
+}
+
+int ModelCreate(ID3D11Device pDev, VOID* pBuff, UINT size, Model* ppModel)
+{
+	return 0;
+}
---
+++ Model/Model.h
@@ -0,0 +1,52 @@
+#pragma once
+
+enum class DRAWTYPE
+{
+	POINTLIST = D3D11_PRIMITIVE_TOPOLOGY_POINTLIST,
+	LINELIST = D3D11_PRIMITIVE_TOPOLOGY_LINELIST,
+	LINESTRIP = D3D11_PRIMITIVE_TOPOLOGY_LINESTRIP,
+	TRIANGLELIST = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST,
+	TRIANGLESTRIP = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP,
+};
+
+class Model
+{
+public:
+	//버텍스 정보
+	struct VTX
+	{
+		float x, y, z;
+	};
+
+protected:
+	ComPtr<ID3D11Device>		m_pDev;
+	ComPtr<ID3D11DeviceContext> m_pDXDC;
+
+protected:
+	const TCHAR* m_Name;						//쉐이더 객체 이름
+
+	ComPtr<ID3D11Buffer> m_pVB[2];				//일단 2개만
+	UINT m_buffSize;							//버퍼 크기
+	UINT m_Stride;								//정점 크기
+	UINT m_Offset;								//렌더링 시작 정점, 대부분 0
+	UINT m_VtxCnt;								//정점 개수, Draw할 때 쓰는거
+
+	ComPtr<ID3D11InputLayout> m_pLayout;		//정점 레이아웃
+
+public:
+	virtual int CreateVB(void* pBuff, UINT vtxCnt);
+	virtual int CreateLayout();
+
+public:
+	Model();
+	virtual ~Model();
+
+	virtual const TCHAR* SetName(const TCHAR* name);
+	virtual const TCHAR* GetName() { return m_Name; }
+
+	virtual int Create(ID3D11Device* pDev, VOID* pBuff, UINT size);
+	virtual int Update(float dTime = 0);
+	virtual int Draw(float dTime = 0, DRAWTYPE drawType = DRAWTYPE::TRIANGLELIST);
+};
+
+int  ModelCreate(ID3D11Device pDev, VOID* pBuff, UINT size, Model* ppModel);
